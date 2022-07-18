c400096ff863cfe9cc15d1bea4b98444b1bc657c
Gurkaran Singh <gurkaran@grevity.in>
2022-07-18 10:25:42 +0530

    Add files via upload

---
+++ reduce_noise.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+current_working_dir = os.getcwd()
+
+for folder in os.listdir(current_working_dir):
+	if os.path.isdir(current_working_dir + "/" + folder) and folder != "Books" and folder != "Book":
+		for mp3_audio in os.listdir(current_working_dir + "/" + folder):
+			if(mp3_audio.endswith('.mp3') or mp3_audio.endswith('.m4a') or mp3_audio.endswith('.aac') or mp3_audio.endswith('.wav') or mp3_audio.endswith('.mp4') or mp3_audio.endswith('.mp4')):
+				os.system('ffmpeg -i "' + current_working_dir + '/' + folder + '/' + mp3_audio + '" -af arnndn=m=bd.rnnn "' + current_working_dir + '/' + folder + '/nr_' + mp3_audio + '"')
+				os.remove(current_working_dir + '/' + folder + '/' + mp3_audio)
+				os.rename(current_working_dir + '/' + folder + '/nr_' + mp3_audio, mp3_audio)
